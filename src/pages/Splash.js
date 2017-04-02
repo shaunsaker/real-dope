963fea4aed8e854fc23c92e0af168d784c52e50c
Shaun Saker <info@shaunsaker.com>
2017-04-02 15:03:44 +0200

    Fix buggy animation on Splash Page

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -32,14 +32,14 @@ export class Splash extends React.Component {
 
       setTimeout(() => {
 
-       // Check if user is authenticated
+        // Check if user is authenticated
         if (!this.props.authenticated) {
           this.props.dispatch({
             type: 'userAuth'
           });
         }
       }, 1500);
-      
+
     }, 1500);
   }
 
@@ -69,7 +69,7 @@ export class Splash extends React.Component {
                 : { top: spring(prevInterpolatedStyles[i - 1].top + 65, presets.wobbly) }
             })}>
             {(interpolatedStyles) => {
-              return <div className='position-relative' style={{ height: '138px' }}>
+              return <div className='position-absolute' style={{ height: '138px' }}>
                 {interpolatedStyles.map((style, index) => {
                   return (
                     <div className='position-absolute-ct-hz' style={{ top: style.top }} key={index}>
@@ -83,17 +83,19 @@ export class Splash extends React.Component {
               </div>
             }}
           </StaggeredMotion>
-          {this.state.isMounted ?
-            <Motion
-              defaultStyle={{ opacity: 0 }}
-              style={{ opacity: spring(1, presets.stiff) }}>
-              {(style) =>
-                <p key="description" className="text-secondary text-light" style={style}>Getting your digits fam...</p>
-              }
-            </Motion>
-            :
-            <p key="test" className="text-secondary text-light no-viz">Getting your digits fam...</p>
-          }
+          <div style={{ position: 'absolute', top: '65%' }}>
+            {this.state.isMounted ?
+              <Motion
+                defaultStyle={{ opacity: 0 }}
+                style={{ opacity: spring(1, presets.stiff) }}>
+                {(style) =>
+                  <p key="description" className="text-secondary text-light" style={style}>Getting your digits fam...</p>
+                }
+              </Motion>
+              :
+              <p key="test" className="text-secondary text-light no-viz">Getting your digits fam...</p>
+            }
+          </div>
         </div>
 
         {!this.props.authenticated && this.state.isMounted ?
